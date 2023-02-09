8cd5db1016ff8d17f8bbdfb6d4f693a1bf875d45
dorcax <dorcasibrahim667@gmail.com>
2023-02-09 08:03:58 +0100

    added Db

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ const express =require("express")
 const app =express()
 const port =process.env.port||8000
 app.use(express.json())
-const Authorlist=[{ id:1,authorname:"kelly morgan",title:'Abusive Wife'}]
-app.get("/api/course",(req,res)=>{
-    res.send(Authorlist)
+const Author =require("./model/authorschema")
+const mongoose=require("mongoose")
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
+// const Authorlist=[{ id:1,authorname:"kelly morgan",title:'Abusive Wife'}]
+app.get("/api/course",async(req,res)=>{
+    const findauthor = await Author.find()
+    res.send(findauthor)
 })
-app.post("/api/course",(req,res)=>{
-    if(!req.body.authorname && !req.body.title) res.status(404).send("please fill your details ")
-    const author ={
-        id:Authorlist.length +1,
-        authorname:req.body.authorname,
-        title:req.body.title
-      
-        
+app.post("/api/course",async(req,res,next)=>{
+    
+    try {
+        const{name,title} =req.body
+        const auth =new Author({name,title})
+        await auth.save()
+        res.send(auth)
+    } catch (error) {
+        next(error)
     }
    
-    Authorlist.push(author)
-    res.send(Authorlist)
+   
+ 
+  
  
     
 })
@@ -28,6 +35,10 @@ app.get("/api/course/:id",(req,res)=>{
     if(!findid) res.status(404).send("The author with the given  Id  is not found")
     
     res.send(findid)
+})
+app.use((req,res,next)=>{
+      
+        res.status(501).send("oh boy something went wrong")
 })
 app.listen(port,()=>{
     console.log(`App listening on port ${port}`)
