49d9bca7394488054703296ec2afe692a1fc24c8
dorcax <98470675+dorcax@users.noreply.github.com>
2023-02-11 11:57:46 +0100

    added new routes

--- server.js
+++ server.js
@@ -3,19 +3,22 @@ const app =express()
 const port =process.env.port||8000
 app.use(express.json())
 const Author =require("./model/authorschema")
+const Book=require("./model/bookschema")
 const mongoose=require("mongoose")
 mongoose.connect('mongodb://127.0.0.1:27017/test')
   .then(() => console.log('Connected!'));
 // const Authorlist=[{ id:1,authorname:"kelly morgan",title:'Abusive Wife'}]
+// author route
+
 app.get("/api/course",async(req,res)=>{
-    const findauthor = await Author.find()
+    const findauthor = await Author.find().populate("books","title")
     res.send(findauthor)
 })
 app.post("/api/course",async(req,res,next)=>{
     
     try {
-        const{name,title} =req.body
-        const auth =new Author({name,title})
+        const{name,description_of_author} =req.body
+        const auth =new Author({name,description_of_author})
         await auth.save()
         res.send(auth)
     } catch (error) {
@@ -28,14 +31,26 @@ app.post("/api/course",async(req,res,next)=>{
  
     
 })
-app.get("/api/course/:id",(req,res)=>{
+app.get("/api/course/:id",async(req,res)=>{
 
     const{id}=req.params
-    const findid = Authorlist.find(c=>c.id ===parseInt(id))
-    if(!findid) res.status(404).send("The author with the given  Id  is not found")
-    
-    res.send(findid)
+     const showauthor = await Author.findById(id).populate("books")
+     res.send(showauthor)
 })
+
+// book route
+app.post("/api/course/:id/books",async(req,res)=>{
+    const{id} =req.params
+    const findauthor= await Author.findById(id)
+    const{title,genre,book_description}=req.body
+    const newbook =new Book({title,genre,book_description})
+    findauthor.books.push(newbook)
+    newbook.authors=findauthor
+    await findauthor.save()
+    await newbook.save()
+    res.redirect("/api/course")
+    })
+
 app.use((req,res,next)=>{
       
         res.status(501).send("oh boy something went wrong")
