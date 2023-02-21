a4229143875f2fbe78ddd6ea6ea45ae3f07cda99
dorcax <dorcasibrahim667@gmail.com>
2023-02-21 08:34:27 +0100

    update route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.use(express.json())
 const Author =require("./model/authorschema")
 const Book=require("./model/bookschema")
 const mongoose=require("mongoose")
+const authorschema = require("./model/authorschema")
 mongoose.connect('mongodb://127.0.0.1:27017/test')
   .then(() => console.log('Connected!'));
 // const Authorlist=[{ id:1,authorname:"kelly morgan",title:'Abusive Wife'}]
@@ -40,16 +41,56 @@ app.get("/api/course/:id",async(req,res)=>{
 
 // book route
 app.post("/api/course/:id/books",async(req,res)=>{
-    const{id} =req.params
-    const findauthor= await Author.findById(id)
-    const{title,genre,book_description}=req.body
-    const newbook =new Book({title,genre,book_description})
-    findauthor.books.push(newbook)
-    newbook.authors=findauthor
-    await findauthor.save()
-    await newbook.save()
+     const{id}=req.params
+     const newAuthor =await Author.findById(id)
+     const {title,book_description,genre} =req.body
+     const newbook =await new Book({title,book_description,genre})
+     newauthor.books.push(newbook)
+     newbook.newAuthor=authors
+     await newauthor.save()
+     await newbook.save()
     res.redirect("/api/course")
     })
+
+app.put("api/course/:id",async (req,res)=>{
+    const{id} =req.params
+    const{title,genre,book_description} =req.body
+    const newbook =await Author.findByIdAndUpdate({
+        _id:new mongoose.Types.ObjectId(id),
+        'books.title':title,
+        
+
+    
+    },
+    {
+        $set:{'books.$.genre':genre,
+              'books.$.book_description':book_description,
+        
+
+    },
+    },{
+        new:true
+    }
+    
+    )
+    res.send("its working")
+     
+    }
+
+)
+
+app.delete("/books/:bookid",async(req,res)=>{
+    const{id,bookid} =req.params
+    const deleteauthor =await Author.findByIdAndUpdate(id,{$pull:{books:bookid}})
+    const deletebook =await Book.findByIdAndDelete(bookid)
+    res.redirect(`/api/course/${id}`)
+})
+// app.delete("/api/course/:id",async(req,res)=>{
+//     const{id} =req.params
+//     const delete_authors = await Author.findByIdAndDelete(id)
+//     res.send(`/api/course`)
+// })
+
 
 app.use((req,res,next)=>{
       
